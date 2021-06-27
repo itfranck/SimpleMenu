50d047e10bdd4aba65dd819d9ed902a0c1a86674
Jean-Frédéric Faust <jffaust@pfdrive.com>
2021-06-26 20:51:35 -0400

    UseTabs switch param

--- Classes/SMBoard.ps1
+++ Classes/SMBoard.ps1
@@ -8,127 +8,132 @@
     [String]$Title
     [INT]$Index
     [INT]$DefaultIndex
+    [bool]$UseTabs
     [psobject]$CurrentActionBoard = $null
 
 
     [Void]Print() {
-    #    ▲△▴▵     ▶▷▸▹ ▼▽▾▿ ◀◁◂◃
-    Clear-Host
+        #    ▲△▴▵     ▶▷▸▹ ▼▽▾▿ ◀◁◂◃
+        Clear-Host
 
-    if ($this.CurrentActionBoard -ne $null) {
-        $CurrentItem = $this.CurrentActionBoard
-    }
-    else {
-        $CurrentItem = $this.Items[$this.Index]
-    }
-    #˂˃˄˅
-    $Arrow1 = '|˂'
-    $Arrow2 = '˃|'
-    $Arrow3 = ''
-    if ($this.Index -gt 0 -or $this.CurrentActionBoard -ne $null)  {$Arrow1 = '˂'}
-    if ($this.Index -lt $this.Items.Count -1 -and $this.CurrentActionBoard -eq $null) {$Arrow2 = '˃'}
-    
-    # The goal is to output something like this:
-    #                                                 -----------------
-    # |    Home    |    Builds    |    Deployments    |    Settings    |    Help    |
-    # -------------------------------------------------                --------------
-
-    $topRowStrings = @()
-    $middleRowStrings = @()
-    $bottomRowStrings = @()
-
-    for($i = 0; $i -lt $this.Items.Count; $i += 1) {
+        if ($this.CurrentActionBoard -ne $null) {
+            $CurrentItem = $this.CurrentActionBoard
+        }
+        else {
+            $CurrentItem = $this.Items[$this.Index]
+        }
+        #˂˃˄˅
+        $Arrow1 = '|˂'
+        $Arrow2 = '˃|'
+        $Arrow3 = ''
+        if ($this.Index -gt 0 -or $this.CurrentActionBoard -ne $null)  {$Arrow1 = '˂'}
+        if ($this.Index -lt $this.Items.Count -1 -and $this.CurrentActionBoard -eq $null) {$Arrow2 = '˃'}
         
-        $padSize = 3
-        $isSelected = $i -eq $this.Index
-        $cellTitle = $this.Items[$i].Title
-        $cellSize = $cellTitle.Length + 2 * $padSize # padding on each side
+        # The goal is to output something like this:
+        #                                                 -----------------
+        # |    Home    |    Builds    |    Deployments    |    Settings    |    Help    |
+        # -------------------------------------------------                --------------
 
-        $topChar = ' '; if ($isSelected) { $topChar = '-' }
-        $topRowStrings += "".PadLeft($cellSize + 1, $topChar)
+        $topRowStrings = @()
+        $middleRowStrings = @()
+        $bottomRowStrings = @()
 
-        $paddedTitle = $cellTitle.PadLeft($cellSize - $padSize, ' ').PadRight($cellSize, ' ')
-        $middleRowStrings += "|$paddedTitle"
+        for($i = 0; $i -lt $this.Items.Count; $i += 1) {
+            
+            $padSize = 3
+            $isSelected = $i -eq $this.Index
+            $cellTitle = $this.Items[$i].Title
+            $cellSize = $cellTitle.Length + 2 * $padSize # padding on each side
 
-        $bottomChar = '-'; if ($isSelected) { $bottomChar = ' ' }
-        $bottomRowStrings += "".PadLeft($cellSize + 1, $bottomChar)
-    }
+            $topChar = ' '; if ($isSelected) { $topChar = '-' }
+            $topRowStrings += "".PadLeft($cellSize + 1, $topChar)
 
-    $topRowString = $topRowStrings -join ''
-    $middleRowString = $middleRowStrings -join ''
-    $bottomRowString = $bottomRowStrings -join ''
+            $paddedTitle = $cellTitle.PadLeft($cellSize - $padSize, ' ').PadRight($cellSize, ' ')
+            $middleRowStrings += "|$paddedTitle"
 
-    $tabs = @"
+            $bottomChar = '-'; if ($isSelected) { $bottomChar = ' ' }
+            $bottomRowStrings += "".PadLeft($cellSize + 1, $bottomChar)
+        }
+
+        $topRowString = $topRowStrings -join ''
+        $middleRowString = $middleRowStrings -join ''
+        $bottomRowString = $bottomRowStrings -join ''
+
+        $tabs = @"
 $topRowString
 $middleRowString|
 $bottomRowString
-`n
 "@
+
+        if ($CurrentItem.menu -ne $null) {
+            $OutTitle = "{0} $($This.Title) — $($CurrentItem.Title) {2} {1}" -f $Arrow1,$Arrow2,$Arrow3
+            $inIndent =   $CurrentItem.Menu.TitleIndent
+            $inActionItems = $CurrentItem.Menu.ActionItems
+            $InTitle = $CurrentItem.Menu.Title
+            $CurrentItem.Menu.TitleIndent = ''
+            
+            $CurrentItem.Menu.Title = $OutTitle
+            if ($this.UseTabs) { $CurrentItem.Menu.Title = $tabs }
+
+            $QuitActionKey = [System.ConsoleKey]::LeftArrow
+            $QuitAction1Key = [System.ConsoleKey]::RightArrow
+            if ([Console]::IsInputRedirected) {
+                $QuitActionKey = [System.ConsoleKey]::A
+                $QuitAction1Key = [System.ConsoleKey]::D
+            }
+
+            $QuitAction = New-SMMenuItem -Key $QuitActionKey -Quit ; $QuitAction.RuntimeKey = $QuitActionKey
+            $QuitAction1 = New-SMMenuItem -Key $QuitAction1Key -Quit ; $QuitAction1.RuntimeKey = $QuitAction1Key
+
+            $CurrentItem.Menu.ActionItems+= $QuitAction
+            $CurrentItem.Menu.ActionItems+= $QuitAction1        
+
+            Invoke-SMMenu $CurrentItem.Menu
+            $CurrentItem.menu.Title = $InTitle
+            $CurrentItem.Menu.TitleIndent = $inIndent
+            $CurrentItem.Menu.ActionItems = $inActionItems
+            switch ($CurrentItem.Menu.QuitKey) {
+                {$_ -in 'LeftArrow','A'} {$this.PreviousBoard() }
+                {$_ -in 'RightArrow','D'} {$this.NextBoard()}
+            }
+            return
+        }
+
+
+        if ($CurrentItem.Pages.Count -gt 1) {
+            $Arrow31 = '˅'
+            $Arrow32 = '˄'
+            
+            switch ($CurrentItem.Index) {
+                0 {$Arrow32 = '-' }
+                {$_ -eq ($CurrentItem.Pages.Count -1)} {$Arrow31 = '-'}
+            }
+            $Arrow3 = " ($($Arrow31)$($Arrow32))"
+        }
+
+        if ($this.UseTabs) { 
+            Write-Host $tabs -ForegroundColor Cyan 
+        } else { 
+            $OutTitle = "{0} $($This.Title) — $($CurrentItem.Title) {2} {1}" -f $Arrow1,$Arrow2,$Arrow3
+            Write-Host $OutTitle  -ForegroundColor Cyan
+        }
     
-    Write-Host $tabs -ForegroundColor Cyan
-
-    if ($CurrentItem.menu -ne $null) {
-        $OutTitle = "{0} $($This.Title) — $($CurrentItem.Title) {2} {1}" -f $Arrow1,$Arrow2,$Arrow3
-        $inIndent =   $CurrentItem.Menu.TitleIndent
-        $inActionItems = $CurrentItem.Menu.ActionItems
-        $InTitle = $CurrentItem.Menu.Title
-        $CurrentItem.Menu.TitleIndent = ''
-        $CurrentItem.Menu.Title = $tabs
-
-        $QuitActionKey = [System.ConsoleKey]::LeftArrow
-        $QuitAction1Key = [System.ConsoleKey]::RightArrow
-        if ([Console]::IsInputRedirected) {
-            $QuitActionKey = [System.ConsoleKey]::A
-            $QuitAction1Key = [System.ConsoleKey]::D
+        $Arguments = @{} 
+        $CurrentPage = $CurrentItem.Pages[$CurrentItem.Index]
+        if ($CurrentItem.ArgumentList -ne $null) {
+            $Arguments.Add('ArgumentList',$CurrentItem.ArgumentList[$CurrentItem.Index])
+        }
+    
+        try {
+            Write-host ($this | Invoke-CommandPiped @Arguments -ScriptBlock ([scriptblock]::Create($CurrentItem.Pages[$CurrentItem.Index])) | Out-String)
+        }
+        catch {
+            Write-Error $_ -ErrorAction Continue
         }
 
-        $QuitAction = New-SMMenuItem -Key $QuitActionKey -Quit ; $QuitAction.RuntimeKey = $QuitActionKey
-        $QuitAction1 = New-SMMenuItem -Key $QuitAction1Key -Quit ; $QuitAction1.RuntimeKey = $QuitAction1Key
-
-        $CurrentItem.Menu.ActionItems+= $QuitAction
-        $CurrentItem.Menu.ActionItems+= $QuitAction1        
-
-        Invoke-SMMenu $CurrentItem.Menu
-        $CurrentItem.menu.Title = $InTitle
-        $CurrentItem.Menu.TitleIndent = $inIndent
-        $CurrentItem.Menu.ActionItems = $inActionItems
-        switch ($CurrentItem.Menu.QuitKey) {
-            {$_ -in 'LeftArrow','A'} {$this.PreviousBoard() }
-            {$_ -in 'RightArrow','D'} {$this.NextBoard()}
+        if (![Console]::IsInputRedirected) {
+            while ([Console]::KeyAvailable) {[console]::ReadKey($false) | Out-Null}
         }
-        return
-    }
-
-
-    if ($CurrentItem.Pages.Count -gt 1) {
-        $Arrow31 = '˅'
-        $Arrow32 = '˄'
-        
-        switch ($CurrentItem.Index) {
-            0 {$Arrow32 = '-' }
-            {$_ -eq ($CurrentItem.Pages.Count -1)} {$Arrow31 = '-'}
-        }
-        $Arrow3 = " ($($Arrow31)$($Arrow32))"
-    }
-
-    $OutTitle = "{0} $($This.Title) — $($CurrentItem.Title) {2} {1}" -f $Arrow1,$Arrow2,$Arrow3
-    Write-Host $OutTitle  -ForegroundColor Cyan
-    $Arguments = @{} 
-    $CurrentPage = $CurrentItem.Pages[$CurrentItem.Index]
-    if ($CurrentItem.ArgumentList -ne $null) {
-        $Arguments.Add('ArgumentList',$CurrentItem.ArgumentList[$CurrentItem.Index])
-    }
-   
-    try {
-        Write-host ($this | Invoke-CommandPiped @Arguments   -ScriptBlock ([scriptblock]::Create($CurrentItem.Pages[$CurrentItem.Index])) | Out-String)
-   }
-   catch {
-       Write-Error $_ -ErrorAction Continue
-   }
-
-    if (![Console]::IsInputRedirected) {
-        while ([Console]::KeyAvailable) {[console]::ReadKey($false) | Out-Null}
-    }
     }
 
     [Void]ChangeDisplayedItem([int]$index) {
